04b1a2e500cf25784ce8c6a7024ba0bd37748679
brendinha <161723534+ybrendx@users.noreply.github.com>
2024-08-19 13:54:29 -0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.ArrayList;
+import java.util;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> Lista = new ArrayList<>();
+        {{
+            Lista.add("vitoria");
+            Lista.add("Daniel");
+            Lista.add("rafael");
+            Lista.add("vanessa");
+            Lista.add("Celine")
+            Lista.add("Sandra");
+        }};
+        System.out.println("tem:"+nome.size());
+
+        {
+            System.out.println(nome.get(2));
+            System.out.println(nome.get(3));
+
+        }
+
+        {
+            System.out.println(nome.indexOf(0));
+            System.out.println(nome.remove("linda"));
+
+        }
+
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+
+
+    }
+}
